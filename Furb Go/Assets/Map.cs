2f20fc726cfca0d4ecc3fa79883585316fb6e090
Leonardo Rovigo <leonardorovigo@gmail.com>
2020-05-12 21:28:40 -0300

    Criado o Mapa da furb com blocos e localidades clicáveis

---
+++ Furb Go/Assets/Map.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+    public Text LabelBlocoSelecionado;
+    public string BlocoSelecionado = string.Empty;
+    public void SetSelected(string bloco)
+    {
+        BlocoSelecionado = bloco;
+        LabelBlocoSelecionado = GameObject.Find("LabelBlocoSelecionado").GetComponent<Text>();
+        LabelBlocoSelecionado.text = $"Você selecionou: {bloco}";
+    }
+}
